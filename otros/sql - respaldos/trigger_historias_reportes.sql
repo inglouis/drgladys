5eb2d1c8f748088f63e425e7d1b6c21165a8879d
marcoxl11 <ldcch2016@gmail.com>
2023-08-03 14:24:21 -0400

    bateria para reiniciar servidor y sistema

---
+++ otros/sql - respaldos/trigger_historias_reportes.sql
@@ -0,0 +1,20 @@
+﻿CREATE OR REPLACE FUNCTION principales.historias_nuevos_reportes()
+  RETURNS trigger AS
+$BODY$
+    BEGIN
+        IF (TG_OP = 'INSERT') THEN
+
+		INSERT INTO principales.reportes(id_historia) values (NEW.id_historia);
+
+		RETURN NEW;
+
+        END IF;
+
+        RETURN NULL;
+    END;
+
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION principales.historias_nuevos_reportes()
+  OWNER TO postgres;
