188b59c04241fd0504484ef78366b83261e076d4
Inglouis <lacrj2011gmail.com>
2023-08-03 16:26:37 -0400

    trigger entre nueva historia y reporte

--- otros/sql - respaldos/trigger_historias_reportes.sql
+++ otros/sql - respaldos/trigger_historias_reportes.sql
@@ -1,20 +1,28 @@
-﻿CREATE OR REPLACE FUNCTION principales.historias_nuevos_reportes()
+﻿CREATE OR REPLACE FUNCTION ppal.historias_reportes_nuevo_renglon()
   RETURNS trigger AS
 $BODY$
-    BEGIN
-        IF (TG_OP = 'INSERT') THEN
-
-		INSERT INTO principales.reportes(id_historia) values (NEW.id_historia);
-
-		RETURN NEW;
-
-        END IF;
-
-        RETURN NULL;
-    END;
-
+BEGIN
+    IF (TG_OP = 'INSERT') THEN
+    --IF NEW.id_historia <> OLD.id_historia THEN
+        INSERT INTO principales.reportes(id_historia)
+        VALUES(NEW.id_historia);
+    END IF;
+    
+    RETURN NEW;
+    
+END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-ALTER FUNCTION principales.historias_nuevos_reportes()
+ALTER FUNCTION ppal.historias_reportes_nuevo_renglon()
   OWNER TO postgres;
+
+
+-----------------------------------------------------------
+DROP TRIGGER historias_reportes_nuevo_renglon on principales.historias;
+
+  CREATE TRIGGER historias_reportes_nuevo_renglon
+  AFTER INSERT
+  ON principales.historias
+  FOR EACH ROW
+  EXECUTE PROCEDURE ppal.historias_reportes_nuevo_renglon();
